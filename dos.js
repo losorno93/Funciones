dd6951fb0eb2b1ee95d1678924e8250e06f618fe
losorno93 <losorno.tamayo@gmail.com>
2022-02-26 17:32:54 -0500

    Realizamos punto 2 correccion

--- dos.js
+++ dos.js
@@ -1,5 +1,5 @@
 //funcion para separar una cadena de texto
 
-let separarcadena=cadenaTexto=>(cadenaTexto.Split(":")[1]
+let separarcadena=cadenaTexto=>(cadenaTexto.split(":")[1])
 //llamando la funcion
 console.log (separarcadena("abc1234:Juan Jose"))
